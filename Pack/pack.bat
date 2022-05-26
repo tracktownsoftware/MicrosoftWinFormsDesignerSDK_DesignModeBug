d29272ea898a8f59773efb0aa41b61049dd565ec
Chris <chris@tracktownsoftware.com>
2022-05-26 11:08:45 -0700

    NET6 WinForms DesignTools application settings bug

--- Pack/pack.bat
+++ Pack/pack.bat
@@ -1,8 +1,5 @@
-set PACKAGEVERSION=1.0.91
+set PACKAGEVERSION=1.0.100
 msbuild ..\MicrosoftWinFormsDesignerSDK_Test1.sln -p:Configuration=Release 
 
 rem build MyButtonLibrary package
 dotnet pack -p:PackageVersion=%PACKAGEVERSION% ..\MyButtonLibrary\MyButtonLibrary.csproj --configuration=Release --output Packages
-
-rem build MyButton2Library package
-dotnet pack -p:PackageVersion=%PACKAGEVERSION% ..\MyButton2Library\MyButton2Library.csproj --configuration=Release --output Packages
